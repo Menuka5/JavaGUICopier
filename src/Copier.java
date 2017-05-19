1130786a997930b5fc9ce9cc9160f641af4ecc94
Menuka <menukaishan5@gmail.com>
2017-05-19 16:55:41 +0530

    First commit of Copier

---
+++ src/Copier.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * Created by hsenid on 5/19/17.
+ */
+public class Copier {
+    private JButton startButton;
+    private JPanel Copier;
+    private JButton stopButton;
+    private JButton pauseButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JCheckBox moveCheckBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
