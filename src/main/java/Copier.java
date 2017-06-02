15fdac080092cdc305396fc5d1b9361f07b517bd
Menuka <menukaishan5@gmail.com>
2017-06-02 09:18:38 +0530

    code refactoring completed.

--- src/main/java/Copier.java
+++ src/main/java/Copier.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,91 +25,80 @@ public class Copier {
 
     public Copier() {
         fileCopyProgressBar.setVisible(false);
-        startButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
 
-                String fromFieldContent = fromField.getText();
-                String toFieldContent = toField.getText();
+        startButton.addActionListener(e -> {
 
-                if (fromFieldContent.equals("") || toFieldContent.equals("")) {
+            String fromFieldContent = fromField.getText();
+            String toFieldContent = toField.getText();
 
-                    String messageForNull = (fromFieldContent.equals("") && toFieldContent.equals("") ? "Both paths are empty" : (fromFieldContent.equals("") ? "From path is Empty" : "To path empty"));
-                    JOptionPane.showMessageDialog(null, messageForNull);
+            if (fromFieldContent.equals("") || toFieldContent.equals("")) {
 
+                String messageForNull = (fromFieldContent.equals("") && toFieldContent.equals("") ? "Both paths are empty" : (fromFieldContent.equals("") ? "From path is Empty" : "To path empty"));
+                JOptionPane.showMessageDialog(null, messageForNull);
+
+            } else {
+                pathOfSource = Paths.get(sourceLocation);
+                pathOfDestination = Paths.get(destinationLocation);
+
+                if (moveCheckBox.isSelected()) {
+
+                    try {
+                        Files.move(pathOfSource, pathOfDestination, StandardCopyOption.REPLACE_EXISTING);
+                        JOptionPane.showMessageDialog(null, "File move Successful!!!");
+                    } catch (IOException e1) {
+                        JOptionPane.showMessageDialog(null, "File move failed!!!");
+                    }
                 } else {
-                    pathOfSource = Paths.get(sourceLocation);
-                    pathOfDestination = Paths.get(destinationLocation);
 
-                    if (moveCheckBox.isSelected()) {
+                    File src = new File(sourceLocation);
+                    File dst = new File(destinationLocation);
 
-                        try {
-                            Files.move(pathOfSource, pathOfDestination, StandardCopyOption.REPLACE_EXISTING);
-                            JOptionPane.showMessageDialog(null, "File move Successful!!!");
-                        } catch (IOException e1) {
-                            JOptionPane.showMessageDialog(null, "File move failed!!!");
-                        }
-                    } else {
-//                        showProgress(fileCopyProgressBar, 76);
-
-                        File src = new File(sourceLocation);
-                        File dst = new File(destinationLocation);
-
-                        try {
-                            dst.createNewFile();
-
+                    try (
                             InputStream in = new FileInputStream(src);
-                            OutputStream out = new FileOutputStream(dst);
-                            long expectedBytes = src.length();
-                            long totalBytesCopied = 0;
-                            byte[] buf = new byte[1024];
-                            int len = 0;
+                            OutputStream out = new FileOutputStream(dst);) {
 
+                        dst.createNewFile();
+                        long expectedBytes = src.length();
+                        long totalBytesCopied = 0;
+                        byte[] buf = new byte[256];
+                        int len = 0;
+                        fileCopyProgressBar.setVisible(true);
 
-                            while ((len = in.read(buf)) > 0) {
+                        while ((len = in.read(buf)) > 0) {
 
-                                out.write(buf, 0, len);
-                                totalBytesCopied += len;
-                                showProgress(totalBytesCopied, expectedBytes);
+                            out.write(buf, 0, len);
+                            totalBytesCopied += len;
+                            showProgress(totalBytesCopied, expectedBytes);
 
-                            }
-
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
                         }
 
-
+                    } catch (IOException ex) {
+                        System.out.println(ex.getMessage());
                     }
                 }
-
-            }
-
-        });
-
-        sourceButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                selectFile = new JFileChooser();
-                selectFile.setDialogTitle("Select a file");
-                selectFile.showDialog(null, "Ok");
-                sourceLocation = selectFile.getSelectedFile().getAbsolutePath();
-                fromField.setText(sourceLocation);
             }
         });
 
-        destinationButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                selectFile = new JFileChooser();
-                selectFile.setDialogTitle("Copy to");
-                selectFile.showDialog(null, "Ok");
-                selectFile.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-                selectFile.setAcceptAllFileFilterUsed(false);
-                destinationLocation = selectFile.getSelectedFile().getAbsolutePath();
-                toField.setText(destinationLocation);
+        sourceButton.addActionListener(e -> {
+            selectFile = new JFileChooser();
+            selectFile.setDialogTitle("Select a file");
+            selectFile.showDialog(null, "Ok");
+            sourceLocation = selectFile.getSelectedFile().getAbsolutePath();
+            fromField.setText(sourceLocation);
+        });
 
-            }
+        destinationButton.addActionListener(e -> {
+            selectFile = new JFileChooser();
+            selectFile.setDialogTitle("Copy to");
+            selectFile.showDialog(null, "Ok");
+            selectFile.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+            selectFile.setAcceptAllFileFilterUsed(false);
+            destinationLocation = selectFile.getSelectedFile().getAbsolutePath();
+            toField.setText(destinationLocation);
+
         });
     }
+
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Copier");
@@ -120,6 +107,7 @@ public class Copier {
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.setVisible(true);
     }
+
 
     public void showProgress(long size, long totalSize) {
 
